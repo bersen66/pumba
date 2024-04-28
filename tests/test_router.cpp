31f559cfd4073e2ddb74d97977adf98ceb0f4ab2
ortur <orturb@yandex.ru>
2024-04-28 21:39:59 +0300

    Release: version 0.1.0

---
+++ tests/test_router.cpp
@@ -0,0 +1,50 @@
+#include <gtest/gtest.h>
+#include <pumba/pumba.hpp>
+
+
+struct MyTraits {
+    using HashType = int;
+
+    static HashType GetHash(const std::string& node) {
+        return std::hash<std::string>{}(node);
+    }
+
+    static std::vector<HashType> Replicate(const std::string& node, std::size_t replicas) {
+        std::vector<HashType> result;
+        result.reserve(replicas);
+
+        for (int i = 0; i < replicas; ++i) {
+            result.emplace_back(GetHash(node + "#" + std::to_string(i)));
+        }
+
+        return result;
+    }
+};
+
+using Router = pumba::ConsistentHashingRouter<std::string, MyTraits>;
+
+TEST(TestRouter, Construction)
+{
+    Router router(/*replicas=*/3);
+    ASSERT_EQ(router.Replicas(), 3);
+    ASSERT_TRUE(router.Empty());
+    ASSERT_EQ(router.PhysicalNodes(), 0);
+}
+
+TEST(TestRouter, Insertion)
+{
+    Router router(/*replicas=*/3);
+    ASSERT_EQ(router.Replicas(), 3);
+    ASSERT_TRUE(router.Empty());
+    ASSERT_EQ(router.PhysicalNodes(), 0);
+
+    router.EmplaceNode("Node");
+    ASSERT_EQ(router.PhysicalNodes(), 1);
+    ASSERT_FALSE(router.Empty());
+
+    for (int i = 0; i < 100; ++i) {
+        std::string node = router.SelectNode("Client" + std::to_string(i));
+        ASSERT_EQ(node, "Node");
+    }
+
+}
